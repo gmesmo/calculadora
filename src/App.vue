1f59425200863902e9b608bebdcf2318faa27ada
GMesmo <guimachado94@gmail.com>
2025-06-11 10:51:31 -0300

    Implementação do exercício

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,52 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { reactive} from "vue"
+
+const estado = reactive({
+  num1: 0,
+  num2: 0,
+  operacao: '+',
+})
+
+function calcula() {
+  const {num1, num2, operacao} = estado;
+  switch (operacao) {
+    case "+":
+      return (num1 + num2);
+    case "-":
+      return num1 - num2;
+    case "X":
+      return num1 * num2;
+    case "/":
+      if (num2 != 0) {
+        return num1 / num2;
+      } else {
+        return ("Erro (divisão por 0)")
+      }
+  }
+    
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="bg-dark w-50 mt-5 py-3 rounded-3 text-center mx-auto text-light">
+      <h1>Calculadora</h1>
+      <div id="container" class="mx-auto w-50">
+        <input class="bg-secondary mx-1 rounded-2 w-25 text-center" 
+          type="number" 
+          v-model.number="estado.num1"
+        >
+        <select class="bg-secondary mx-1 rounded-2 text-center" @change="evento => estado.operacao = evento.target.value">
+          <option value="+">+</option>
+          <option value="-">-</option>
+          <option value="X">X</option>
+          <option value="/">/</option>
+        </select>
+        <input class="bg-secondary mx-1 rounded-2 w-25 text-center" 
+          type="number"
+          v-model.number="estado.num2"
+        >
+      </div>
+      <p class="mt-3">Resultado: {{ calcula() }} </p>
+  </div>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
